84f7493a80461b756685cfe8f1cf4b62eb593ad7
mahin725 <rihanthedev@gmail.com>
2026-05-04 16:59:49 +0600

    authService- onnecxt Db

--- auth-service/src/server.js
+++ auth-service/src/server.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 5001;
 const cors = require('cors');
 const router = require('./routes/auth.routes');
+const connectDB = require('./config/db');
 
+connectDB();
 
 app.use(cors());
 app.use(express.json());
