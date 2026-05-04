673bf56f822d2e260862ab241609d2238a940abc
mahin725 <rihanthedev@gmail.com>
2026-05-04 14:34:23 +0600

    AuthService_password hashing

--- auth-service/src/model/user.model.js
+++ auth-service/src/model/user.model.js
@@ -1,35 +1,46 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
 
-    password: {
-      type: String,
-      required: true,
-      minlength: 6,
-    },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
 
-    role: {
-      type: String,
-      enum: ["admin", "hr", "employee"],
-      default: "employee",
-    },
+        role: {
+            type: String,
+            enum: ["admin", "hr", "employee"],
+            default: "employee",
+        },
 
-    isActive: {
-      type: Boolean,
-      default: true,
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
     },
-  },
-  {
-    timestamps: true, 
-  }
+    {
+        timestamps: true,
+    }
 );
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+// 🔥 PASSWORD COMPARE (login time)
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
